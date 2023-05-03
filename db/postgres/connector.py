bf8dabb826e2eada36c53a57db466aaf3128bbf3
julikss <julie.lehenka@gmail.com>
2023-05-03 22:58:57 +0300

    Small changes

--- db/postgres/connector.py
+++ db/postgres/connector.py
@@ -6,33 +6,35 @@ class PostgresDB:
     
     def read(self):
         cur = self.conn.cursor()
-        cur.execute("SELECT * FROM decanat1")
+        cur.execute("SELECT * FROM db1")
         records = cur.fetchall()
+        cur.close()
         return records
     
     def create(self, data):
         cur = self.conn.cursor()
-        cur.execute("INSERT INTO decanat1 (student_id, course, group_name, student, subject) VALUES (%s, %s, %s, %s, %s)", data)
+        cur.execute("INSERT INTO db1 (student_id, course, group_name, student, subject) VALUES (%s, %s, %s, %s, %s)", data)
         cur.close()
         print("CREATED")
 
     def update(self, data):
         cur = self.conn.cursor()
-        cur.execute("UPDATE decanat1 SET course = %s, group_name = %s, student = %s, subject = %s WHERE student_id = %s", 
+        cur.execute("UPDATE db1 SET course = %s, group_name = %s, student = %s, subject = %s WHERE student_id = %s", 
                     (data[1], data[2], data[3], data[4], data[0]))
         cur.close()
         print("UPDATED")
     
     def delete(self, id):
         cur = self.conn.cursor()
-        cur.execute("DELETE FROM decanat1 WHERE student_id = %s", [id])
+        cur.execute("DELETE FROM db1 WHERE student_id = %s", [id])
         cur.close()
         print("DELETED")
     
     def get_student(self, id):
         cur = self.conn.cursor()
-        cur.execute("SELECT * FROM decanat1 WHERE student_id = %s", [id])
+        cur.execute("SELECT * FROM db1 WHERE student_id = %s", [id])
         student = cur.fetchall()
+        cur.close()
         return student
 
 
